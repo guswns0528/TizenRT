e553562e9c56e38140de198af0f06c73edf101c0
Wonwoo Choi <chwo9843@gmail.com>
2017-06-07 15:40:55 +0900

    add gpioconfig gpiounconfig functions

--- os/arch/arm/src/stm32f4/stm32f4_gpio.c
+++ os/arch/arm/src/stm32f4/stm32f4_gpio.c
@@ -112,4 +112,250 @@ const uint32_t g_gpiobase[STM32_NGPIO_PORTS] =
  * Public Functions
  ****************************************************************************/
 
+void stm32_gpioinit(void)
+{
+}
+
+/****************************************************************************
+ * Name: stm32f4_configgpio
+ *
+ * Description:
+ *   Configure a GPIO pin based on bit-encoded description of the pin.
+ *   Once it is configured as Alternative (GPIO_ALT|GPIO_CNF_AFPP|...)
+ *   function, it must be unconfigured with stm32f4_unconfiggpio() with
+ *   the same cfgset first before it can be set to non-alternative function.
+ *
+ * Returns:
+ *   OK on success
+ *   A negated errono valu on invalid port, or when pin is locked as ALT
+ *   function.
+ *
+ * To-Do: Auto Power Enable
+ ****************************************************************************/
+
+int stm32f4_configgpio(uint32_t cfgset)
+{
+    uintptr_t base;
+    uint32_t regval;
+    uint32_t setting;
+    unsigned int regoffset;
+    unsigned int port;
+    unsigned int pin;
+    unsigned int pos;
+    unsigned int pinmode;
+    irqstate_t flags;
+
+    /* Verify that this hardware supports the select GPIO port */
+
+    port = (cfgset & GPIO_PORT_MASK) >> GPIO_PORT_SHIFT;
+    if (port >= STM32_NGPIO_PORTS)
+    {
+        return -EINVAL;
+    }
+
+    /* Get the port base address */
+
+    base = g_gpiobase[port];
+
+    /* Get the pin number and select the port configuration register for that
+     * pin
+     */
+
+    pin = (cfgset & GPIO_PIN_MASK) >> GPIO_PIN_SHIFT;
+
+    /* Set up the mode register (and remember whether the pin mode) */
+
+    switch (cfgset & GPIO_MODE_MASK)
+    {
+        default:
+        case GPIO_INPUT:      /* Input mode */
+            pinmode = GPIO_MODER_INPUT;
+            break;
+
+        case GPIO_OUTPUT:     /* General purpose output mode */
+            stm32f4_gpiowrite(cfgset, (cfgset & GPIO_OUTPUT_SET) != 0); /* Set the initial output value */
+            pinmode = GPIO_MODER_OUTPUT;
+            break;
+
+        case GPIO_ALT:        /* Alternate function mode */
+            pinmode = GPIO_MODER_ALT;
+            break;
+
+        case GPIO_ANALOG:     /* Analog mode */
+            pinmode = GPIO_MODER_ANALOG;
+            break;
+    }
+
+    /* Interrupts must be disabled from here on out so that we have mutually
+     * exclusive access to all of the GPIO configuration registers.
+     */
+
+    flags = irqsave();
+
+    /* Now apply the configuration to the mode register */
+
+    regval  = getreg32(base + STM32_GPIO_MODER_OFFSET);
+    regval &= ~GPIO_MODER_MASK(pin);
+    regval |= ((uint32_t)pinmode << GPIO_MODER_SHIFT(pin));
+    putreg32(regval, base + STM32_GPIO_MODER_OFFSET);
+
+    /* Set up the pull-up/pull-down configuration (all but analog pins) */
+
+    setting = GPIO_PUPDR_NONE;
+    if (pinmode != GPIO_MODER_ANALOG)
+    {
+        switch (cfgset & GPIO_PUPD_MASK)
+        {
+            default:
+            case GPIO_FLOAT:      /* No pull-up, pull-down */
+                break;
+
+            case GPIO_PULLUP:     /* Pull-up */
+                setting = GPIO_PUPDR_PULLUP;
+                break;
+
+            case GPIO_PULLDOWN:   /* Pull-down */
+                setting = GPIO_PUPDR_PULLDOWN;
+                break;
+        }
+    }
+
+    regval  = getreg32(base + STM32_GPIO_PUPDR_OFFSET);
+    regval &= ~GPIO_PUPDR_MASK(pin);
+    regval |= (setting << GPIO_PUPDR_SHIFT(pin));
+    putreg32(regval, base + STM32_GPIO_PUPDR_OFFSET);
+
+    /* Set the alternate function (Only alternate function pins) */
+
+    if (pinmode == GPIO_MODER_ALT)
+    {
+        setting = (cfgset & GPIO_AF_MASK) >> GPIO_AF_SHIFT;
+    }
+    else
+    {
+        setting = 0;
+    }
+
+    if (pin < 8)
+    {
+        regoffset = STM32_GPIO_AFRL_OFFSET;
+        pos       = pin;
+    }
+    else
+    {
+        regoffset = STM32_GPIO_AFRH_OFFSET;
+        pos       = pin - 8;
+    }
+
+    regval  = getreg32(base + regoffset);
+    regval &= ~GPIO_AFR_MASK(pos);
+    regval |= (setting << GPIO_AFR_SHIFT(pos));
+    putreg32(regval, base + regoffset);
+
+    /* Set speed (Only outputs and alternate function pins) */
+
+    if (pinmode == GPIO_MODER_OUTPUT || pinmode == GPIO_MODER_ALT)
+    {
+        switch (cfgset & GPIO_SPEED_MASK)
+        {
+            default:
+            case GPIO_SPEED_2MHz:    /* 2 MHz Low speed output */
+                setting = GPIO_OSPEED_2MHz;
+                break;
+
+            case GPIO_SPEED_25MHz:   /* 25 MHz Medium speed output */
+                setting = GPIO_OSPEED_25MHz;
+                break;
+
+            case GPIO_SPEED_50MHz:   /* 50 MHz Fast speed output  */
+                setting = GPIO_OSPEED_50MHz;
+                break;
+
+            case GPIO_SPEED_100MHz:   /* 100 MHz High speed output */
+                setting = GPIO_OSPEED_100MHz;
+                break;
+        }
+    }
+    else
+    {
+        setting = 0;
+    }
+
+    regval  = getreg32(base + STM32_GPIO_OSPEED_OFFSET);
+    regval &= ~GPIO_OSPEED_MASK(pin);
+    regval |= (setting << GPIO_OSPEED_SHIFT(pin));
+    putreg32(regval, base + STM32_GPIO_OSPEED_OFFSET);
+
+    /* Set push-pull/open-drain (Only outputs and alternate function pins) */
+
+    regval  = getreg32(base + STM32_GPIO_OTYPER_OFFSET);
+    setting = GPIO_OTYPER_OD(pin);
+
+    if ((pinmode == GPIO_MODER_OUTPUT || pinmode == GPIO_MODER_ALT) &&
+            (cfgset & GPIO_OPENDRAIN) != 0)
+    {
+        regval |= setting;
+    }
+    else
+    {
+        regval &= ~setting;
+    }
+
+    putreg32(regval, base + STM32_GPIO_OTYPER_OFFSET);
+
+    /* Otherwise, it is an input pin.  Should it configured as an EXTI interrupt? */
+
+    if (pinmode != GPIO_MODER_OUTPUT && (cfgset & GPIO_EXTI) != 0)
+    {
+        uint32_t regaddr;
+        uint8_t regval8;
+        int shift;
+
+        /* Set the bits in the SYSCFG EXTICR register */
+
+        regaddr = STM32_SYSCFG_EXTICR(pin);
+        regval8 = getreg8(regaddr);
+        shift   = SYSCFG_EXTICR_EXTI_SHIFT(pin);
+        regval8 &= ~(SYSCFG_EXTICR_PORT_MASK << shift);
+        regval8 |= (((uint32_t)port) << shift);
+
+        putreg8(regval, regaddr);
+    }
+
+    irqrestore(flags);
+    return OK;
+}
+
+/****************************************************************************
+ * Name: stm32f4_unconfiggpio
+ *
+ * Description:
+ *   Unconfigure a GPIO pin based on bit-encoded description of the pin, set it
+ *   into default HiZ state (and possibly mark it's unused) and unlock it whether
+ *   it was previsouly selected as alternative function (GPIO_ALT|GPIO_CNF_AFPP|...).
+ *
+ *   This is a safety function and prevents hardware from schocks, as unexpected
+ *   write to the Timer Channel Output GPIO to fixed '1' or '0' while it should
+ *   operate in PWM mode could produce excessive on-board currents and trigger
+ *   over-current/alarm function.
+ *
+ * Returns:
+ *  OK on success
+ *  A negated errno value on invalid port
+ *
+ * To-Do: Auto Power Disable
+ ****************************************************************************/
+
+int stm32f4_unconfiggpio(uint32_t cfgset)
+{
+    /* Reuse port and pin number and set it to default HiZ INPUT */
+
+    cfgset &= GPIO_PORT_MASK | GPIO_PIN_MASK;
+    cfgset |= GPIO_INPUT | GPIO_FLOAT;
+
+    /* To-Do: Mark its unuse for automatic power saving options */
+
+    return stm32f4_configgpio(cfgset);
+}
+
 
