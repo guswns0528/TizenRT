30a35b4c11c2ba9d260c1ad2936ab3bbbff23177
Joon <guswns0528@gmail.com>
2017-06-14 05:20:21 +0900

    add halt_loop for debug

--- os/arch/arm/src/stm32f4/stm32f4_boot.c
+++ os/arch/arm/src/stm32f4/stm32f4_boot.c
@@ -1,21 +1,25 @@
-extern void up_init_lowled();
-extern void toggle_led();
+#include <tinyara/config.h>
+#include <stdint.h>
 
-void halt_loop()
+#include "up_internal.h"
+
+extern void up_init_lowled(void);
+extern void toggle_led(void);
+extern void stm32_clockconfig(void);
+extern void up_lowputc(char ch);
+
+void halt_loop(void)
 {
     while (1)
     {
         int i;
-        for (i = 0; i < 100000; i++)
+        for (i = 0; i < 16800000 / 7; i++)
         {
         }
         toggle_led();
     }
 }
 
-void arm_boot()
+void arm_boot(void)
 {
-    up_init_lowled();
-
-    halt_loop();
 }
