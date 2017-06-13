60337a6ec9b895f2bb8b05c466ef4563e03e6dc1
Joon <guswns0528@gmail.com>
2017-06-14 03:33:09 +0900

    initialize timer interrupt using systick

--- os/arch/arm/src/stm32f4/stm32f4_timerisr.c
+++ os/arch/arm/src/stm32f4/stm32f4_timerisr.c
@@ -104,3 +104,56 @@ static int stm32_timerisr(int irq, uint32_t *regs, void *arg)
     return 0;
 }
 
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Function:  arm_timer_initialize
+ *
+ * Description:
+ *   This function is called during start-up to initialize
+ *   the timer interrupt.
+ *
+ ****************************************************************************/
+
+void up_timer_initialize(void)
+{
+    uint32_t regval;
+
+    /* Set the SysTick interrupt to the default priority */
+
+    regval = getreg32(NVIC_SYSH12_15_PRIORITY);
+    regval &= ~NVIC_SYSH_PRIORITY_PR15_MASK;
+    regval |= (NVIC_SYSH_PRIORITY_DEFAULT << NVIC_SYSH_PRIORITY_PR15_SHIFT);
+    putreg32(regval, NVIC_SYSH12_15_PRIORITY);
+
+    /* Make sure that the SYSTICK clock source is set correctly */
+
+#if 0 /* Does not work.  Comes up with HCLK source and I can't change it */
+    regval = getreg32(NVIC_SYSTICK_CTRL);
+#ifdef CONFIG_STM32_SYSTICK_HCLKd8
+    regval &= ~NVIC_SYSTICK_CTRL_CLKSOURCE;
+#else
+    regval |= NVIC_SYSTICK_CTRL_CLKSOURCE;
+#endif
+    putreg32(regval, NVIC_SYSTICK_CTRL);
+#endif
+
+    /* Configure SysTick to interrupt at the requested rate */
+
+    putreg32(SYSTICK_RELOAD, NVIC_SYSTICK_RELOAD);
+
+    /* Attach the timer interrupt vector */
+
+    (void)irq_attach(STM32_IRQ_SYSTICK, (xcpt_t)stm32_timerisr, NULL);
+
+    /* Enable SysTick interrupts */
+
+    putreg32((NVIC_SYSTICK_CTRL_CLKSOURCE | NVIC_SYSTICK_CTRL_TICKINT |
+                NVIC_SYSTICK_CTRL_ENABLE), NVIC_SYSTICK_CTRL);
+
+    /* And enable the timer interrupt */
+
+    up_enable_irq(STM32_IRQ_SYSTICK);
+}
